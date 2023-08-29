c1a2b8486ac93cc4a9a0dc7c56840a89f64b48ed
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-08-29 15:19:56 +0200

    refactor: set base URI

--- vite.config.ts
+++ vite.config.ts
@@ -2,41 +2,44 @@ import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import vue from "@vitejs/plugin-vue";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: "./src/main/webapp",
-  envDir: "../../../",
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.includes("extended-uportal-"),
+// @ts-ignore
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: process.env.VITE_BASE_URI + "/ui",
+    root: "./src/main/webapp",
+    envDir: "../../../",
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.includes("extended-uportal-"),
+          },
+        },
+      }),
+      vuetify({ autoImport: true }),
+      VueI18nPlugin({
+        include: resolve(dirname(fileURLToPath(import.meta.url)), "./src/main/webapp/src/locales/**"),
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src/main/webapp/src", import.meta.url)),
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/glc-[name].[ext]",
+          entryFileNames: "assets/glc-[name].js",
+          chunkFileNames: "assets/glc-[name].js",
         },
       },
-    }),
-    vuetify({ autoImport: true }),
-    VueI18nPlugin({
-      include: resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        "./src/main/webapp/src/locales/**"
-      ),
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src/main/webapp/src", import.meta.url)),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/glc-[name].[ext]",
-        entryFileNames: "assets/glc-[name].js",
-        chunkFileNames: "assets/glc-[name].js",
-      },
-    },
-  },
-});
+  });
+};
