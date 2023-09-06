808798853e59fb62be530dc5401e60ebd7fdeae5
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-09-06 11:03:49 +0200

    fix: add axios timeout

--- src/main/webapp/src/utils/axiosUtils.ts
+++ src/main/webapp/src/utils/axiosUtils.ts
@@ -10,6 +10,7 @@ const { VITE_API_URL } = import.meta.env;
 
 const instance = axios.create({
   baseURL: VITE_API_URL,
+  timeout: 10000,
   withCredentials: true,
   xsrfCookieName: 'CSRF-TOKEN',
   xsrfHeaderName: 'X-CSRF-TOKEN',
