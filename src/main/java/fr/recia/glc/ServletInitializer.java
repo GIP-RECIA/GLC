61fa11ed2cf45ec4f494a85ca55324276e2d59f4
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-10-17 10:02:45 +0200

    refactor: update ServletInitializer

--- src/main/java/fr/recia/glc/ServletInitializer.java
+++ src/main/java/fr/recia/glc/ServletInitializer.java
@@ -15,14 +15,32 @@
  */
 package fr.recia.glc;
 
+import fr.recia.glc.configuration.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Slf4j
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(GlcApplication.class);
+    return application
+      .profiles(addDefaultProfile())
+      .bannerMode(Banner.Mode.OFF)
+      .sources(GlcApplication.class);
+  }
+
+  private String addDefaultProfile() {
+    String profile = System.getProperty("spring.profiles.active");
+    if (profile != null) {
+      log.info("Running with Spring profile(s) : {}", profile);
+      return profile;
+    }
+
+    log.warn("No Spring profile configured, running with default configuration");
+    return Constants.SPRING_PROFILE_DEVELOPMENT;
   }
 
 }
--- src/main/java/fr/recia/glc/configuration/Constants.java
+++ src/main/java/fr/recia/glc/configuration/Constants.java
@@ -17,10 +17,18 @@ package fr.recia.glc.configuration;
 
 public class Constants {
 
+  private Constants() {
+    throw new IllegalStateException("Utility class");
+  }
+
   public static final String SARAPISUI_ = "SarapisUi_";
   public static final String SANS_OBJET = "SANS OBJET";
   public static final String PROPERTIES_TO_JSON_DELIMITER = "\", \"";
   public static final String PROPERTIES_TO_JSON_PREFIX = "[ \"";
   public static final String PROPERTIES_TO_JSON_SUFFIX = "\" ]";
+
+  public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+  public static final String SPRING_PROFILE_PRODUCTION = "prod";
+  public static final String SPRING_PROFILE_TEST = "test";
 
 }
