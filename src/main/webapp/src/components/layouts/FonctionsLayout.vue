30ab72199d60be874de3ffcd7eb9514b6c44f90c
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-12-07 15:16:55 +0100

    refactor: optimize FonctionsLayout

--- src/main/webapp/src/components/layouts/FonctionsLayout.vue
+++ src/main/webapp/src/components/layouts/FonctionsLayout.vue
@@ -3,7 +3,7 @@ import { useConfigurationStore } from '@/stores/configurationStore.ts';
 import type { Filiere } from '@/types/filiereType.ts';
 import type { PersonneFonction } from '@/types/fonctionType.ts';
 import { storeToRefs } from 'pinia';
-import { onBeforeMount, ref, unref, watch } from 'vue';
+import { ref, watch } from 'vue';
 
 const configurationStore = useConfigurationStore();
 const { currentStructureId } = storeToRefs(configurationStore);
@@ -15,9 +15,21 @@ const props = defineProps<{
 
 const filteredFilieres = ref<Array<Filiere>>([]);
 
-onBeforeMount(() => {
-  if (props.fonctions && props.fonctions.length > 0) filterFilieres();
-});
+const filterFilieres = (): void => {
+  if (props.filieres && props.fonctions) {
+    const etabFonctions = props.fonctions.filter((fonction) => fonction.structure == currentStructureId.value);
+    const filiereIds = [...new Set(etabFonctions.map((fonction) => fonction.filiere))];
+    const disciplineIds = [...new Set(etabFonctions.map((fonction) => fonction.disciplinePoste))];
+
+    filteredFilieres.value = props.filieres
+      .filter((filiere) => filiereIds.includes(filiere.id))
+      .map((filiere) => {
+        const disciplines = filiere.disciplines.filter((discipline) => disciplineIds.includes(discipline.id));
+
+        return { ...filiere, disciplines };
+      });
+  }
+};
 
 watch(
   () => props.fonctions,
@@ -26,47 +38,29 @@ watch(
       filterFilieres();
     }
   },
+  { immediate: true },
 );
-
-const filterFilieres = (): void => {
-  const etabFonctions = props.fonctions
-    ? props.fonctions.filter((fonction) => fonction.structure == currentStructureId.value)
-    : [];
-  const filiereIds = [...new Set(etabFonctions.map((fonction) => fonction.filiere))];
-  const disciplineIds = [...new Set(etabFonctions.map((fonction) => fonction.disciplinePoste))];
-
-  let filterFilieres = unref(props.filieres);
-
-  filterFilieres = filterFilieres
-    ?.filter((filiere) => filiereIds.includes(filiere.id))
-    .map((filiere) => {
-      const disciplines = filiere.disciplines.filter((discipline) => disciplineIds.includes(discipline.id));
-
-      return { ...filiere, disciplines };
-    });
-
-  filteredFilieres.value = filterFilieres ? filterFilieres : [];
-};
 </script>
 
 <template>
-  <div v-if="fonctions && fonctions.length > 0 && filteredFilieres.length > 0">
-    <v-row>
-      <v-col v-for="(filiere, index) in filteredFilieres" :key="index" :cols="12" :md="6" class="pa-2">
-        <v-card :subtitle="filiere.libelleFiliere" variant="tonal" min-height="100%">
-          <v-card-text class="pb-2 mt--3">
-            <div class="v-chip-group v-chip-group--column v-theme--light">
-              <v-chip
-                v-for="(discipline, index) in filiere.disciplines"
-                :key="index"
-                :text="discipline.disciplinePoste"
-                color="primary"
-                rounded
-              />
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-row v-if="filteredFilieres.length > 0">
+    <v-col v-for="(filiere, index) in filteredFilieres" :key="index" :cols="12" :md="6" class="pa-2">
+      <v-card :subtitle="filiere.libelleFiliere" variant="tonal" min-height="100%">
+        <v-card-text class="pb-2 mt--3">
+          <div class="v-chip-group v-chip-group--column v-theme--light">
+            <v-chip
+              v-for="(discipline, index) in filiere.disciplines"
+              :key="index"
+              :text="discipline.disciplinePoste"
+              color="primary"
+              rounded
+            />
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+  <div v-else>
+    <v-slot name="empty" />
   </div>
 </template>
