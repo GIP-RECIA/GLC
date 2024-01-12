8628e06e134f7c7903c8892f912b248e11479cb9
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-01-12 10:01:07 +0100

    fix: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
 
-init () {
-  read -r -p "Initialize environment? [y/N] " initialize
-  initialize=${initialize,,}
-
-  if [[ "$initialize" =~ ^(yes|y)$ ]]; then
-    echo "--- Initialize ---"
-    source ./scripts/init.sh
-  fi
-}
-
-build () {
-  read -r -p "Build frontend? [y/N] " frontend
-  frontend=${frontend,,}
-
-  if [[ "$frontend" =~ ^(yes|y)$ ]]; then
-    echo "--- Build frontend ---"
-    if ! yarn build; then exit; fi
-  fi
-}
-
 package () {
   echo "--- Package ---"
-  ./mvnw clean package -P prod -Dmaven.test.skip=true -Darguments="-DskipTests -Dmaven.deploy.skip=true"
+  ./mvnw clean package -P prod -Dmaven.test.skip=true -Darguments="-DskipTests"
 }
 
 snapshot () {
   echo "--- Snapshot ---"
-  ./mvnw clean package deploy -P prod -Dmaven.test.skip=true -Darguments="-DskipTests -Dmaven.deploy.skip=true"
+  ./mvnw clean package deploy -P prod -Dmaven.test.skip=true -Darguments="-DskipTests"
 }
 
 release () {
   echo "--- Release ---"
-  ./mvnw clean package release:prepare release:perform -P prod -Dmaven.test.skip=true -Darguments="-DskipTests -Dmaven.deploy.skip=true"
+  ./mvnw clean release:prepare release:perform -P prod -Dmaven.test.skip=true -Darguments="-DskipTests"
 }
 
 echo "GLC Building Tool"
@@ -47,20 +27,14 @@ read -r -p "Build type: " choice
 case $choice in
 
   0)
-    init
-    build
     package
   ;;
 
   1)
-    init
-    build
     snapshot
   ;;
 
   2)
-    init
-    build
     release
   ;;
 
