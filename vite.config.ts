6673db7138c5fc082b7333a1255151a2f9507919
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-01-30 12:32:43 +0100

    build: fix hmr

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,9 @@ export default ({ mode }: { mode: string }) => {
           changeOrigin: true,
         },
       },
+      hmr: {
+        path: 'ws',
+      },
     },
     build: {
       rollupOptions: {
