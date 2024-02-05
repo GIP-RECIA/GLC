253fe20ffeb2fc02718ea3329a66c9ddc2cc424f
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-02-05 16:35:26 +0100

    build: improve app slug definition

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { slugify } from './src/main/webapp/src/utils/stringUtils.ts';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import vue from '@vitejs/plugin-vue';
 import { readFileSync } from 'node:fs';
@@ -11,25 +12,9 @@ import { parseString } from 'xml2js';
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  const slugify = (value?: string): string => {
-    return JSON.stringify(
-      String(value)
-        .normalize('NFKD') // split accented characters into their base characters and diacritical marks
-        .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-        .trim() // trim leading or trailing whitespace
-        .toLowerCase() // convert to lowercase
-        .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
-        .replace(/\s+/g, '-') // replace spaces with hyphens
-        .replace(/-+/g, '-'), // remove consecutive hyphens
-    );
-  };
-
   const appName = JSON.stringify(process.env.VITE_APP_NAME);
 
-  const appSlug = (): string => {
-    if (process.env.VITE_APP_SLUG) return JSON.stringify(process.env.VITE_APP_SLUG);
-    else return slugify(process.env.VITE_APP_NAME);
-  };
+  const appSlug = JSON.stringify(process.env.VITE_APP_SLUG ? process.env.VITE_APP_SLUG : slugify(appName));
 
   const backVersion = (): string => {
     let version;
@@ -90,7 +75,7 @@ export default ({ mode }: { mode: string }) => {
     },
     define: {
       __APP_NAME__: appName,
-      __APP_SLUG__: appSlug(),
+      __APP_SLUG__: appSlug,
       __BACK_VERSION__: backVersion(),
     },
   });
