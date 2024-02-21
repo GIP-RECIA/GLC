191be0d45548f8145152c94772196b8f6ba33f5e
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-02-21 14:14:40 +0100

    fix: missing translations

--- src/main/webapp/src/locales/en/index.ts
+++ src/main/webapp/src/locales/en/index.ts
@@ -2,10 +2,8 @@ import additionals from './additionals.json';
 import alerts from './alerts.json';
 import buttons from './buttons.json';
 import main from './main.json';
-import offices from './offices.json';
 import persons from './persons.json';
 import searchs from './searchs.json';
-import sources from './sources.json';
 import tabs from './tabs.json';
 import toasts from './toasts.json';
 import { en } from 'vuetify/locale';
@@ -15,10 +13,8 @@ export default {
   ...alerts,
   ...buttons,
   ...main,
-  ...offices,
   ...persons,
   ...searchs,
-  ...sources,
   ...tabs,
   ...toasts,
   $vuetify: {
