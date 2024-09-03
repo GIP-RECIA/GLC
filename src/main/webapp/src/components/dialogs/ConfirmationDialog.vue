b941b09f8a41345d81592778b8317d1d4e46318e
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-09-03 10:42:03 +0200

    feat: add confirmation dialog

---
+++ src/main/webapp/src/components/dialogs/ConfirmationDialog.vue
@@ -0,0 +1,78 @@
+<!--
+ Copyright (C) 2023 GIP-RECIA, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<script setup lang="ts">
+import type { Confirmation } from '@/types';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+withDefaults(
+  defineProps<{
+    title: string;
+    description?: string;
+    yesValue: string;
+    noValue: string;
+    cancelable?: boolean;
+    noColor?: string;
+    yesColor?: string;
+    maxWidth?: string | number;
+  }>(),
+  {
+    cancelable: false,
+    maxWidth: 500,
+  },
+);
+
+const modelValue = defineModel<boolean>();
+
+const emit = defineEmits({
+  close(payload: Confirmation): boolean {
+    return !!payload;
+  },
+});
+
+const onCancel = (): void => {
+  modelValue.value = false;
+  emit('close', 'cancel');
+};
+
+const onNo = (): void => {
+  modelValue.value = false;
+  emit('close', 'no');
+};
+
+const onYes = (): void => {
+  modelValue.value = false;
+  emit('close', 'yes');
+};
+</script>
+
+<template>
+  <v-dialog v-model="modelValue" persistent :max-width="maxWidth">
+    <v-card :title="title" rounded="xl">
+      <v-card-text v-if="description">{{ description }}</v-card-text>
+      <v-card-text v-else-if="$slots.description"><slot name="description" /></v-card-text>
+
+      <v-card-actions>
+        <v-btn v-if="cancelable" :text="t('button.cancel')" @click="onCancel" />
+        <v-spacer />
+        <v-btn :text="t(noValue)" :color="noColor" @click="onNo" />
+        <v-btn :text="t(yesValue)" :color="yesColor" @click="onYes" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
