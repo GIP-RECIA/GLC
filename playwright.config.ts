a3c367632eec234df8268a0e3485fe35fa6f227b
Quentin Guillemin <quentin.guillemin@recia.fr>
2026-03-03 11:00:36 +0100

    chore: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
+import process from 'node:process'
+import { defineConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-dotenv.config();
+dotenv.config()
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -34,7 +35,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.TEST_URL,
+    baseURL: process.env.CI ? 'http://localhost:4173' : 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -58,9 +59,9 @@ export default defineConfig({
       },
     },
     // {
-    //   name: "webkit",
+    //   name: 'webkit',
     //   use: {
-    //     ...devices["Desktop Safari"],
+    //     ...devices['Desktop Safari'],
     //   },
     // },
 
@@ -101,10 +102,10 @@ export default defineConfig({
     /**
      * Use the dev server by default for faster feedback loop.
      * Use the preview server on CI for more realistic testing.
-    Playwright will re-use the local server if there is already a dev-server running.
+     * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
-    port: 5173,
+    command: process.env.CI ? 'yarn run preview' : 'yarn run dev',
+    port: process.env.CI ? 4173 : 5173,
     reuseExistingServer: !process.env.CI,
   },
-});
+})
