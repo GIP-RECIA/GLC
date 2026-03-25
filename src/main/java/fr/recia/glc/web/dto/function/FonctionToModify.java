723e9a4d2aa1fa61e0b17082ef675dcb22ee55e2
cailbourdin <nathan.cailbourdin@recia.fr>
2026-03-25 17:25:24 +0100

    feat(wip): ajout dateFin dans les modifications de fonction

--- src/main/java/fr/recia/glc/web/dto/function/FonctionToModify.java
+++ src/main/java/fr/recia/glc/web/dto/function/FonctionToModify.java
@@ -2,8 +2,11 @@ package fr.recia.glc.web.dto.function;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class FonctionToModify {
     private Long filiere;
     private Long discipline;
+    private Date dateFin;
 }
