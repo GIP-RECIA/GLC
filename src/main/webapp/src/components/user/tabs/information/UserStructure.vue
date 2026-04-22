c8304c3effc163d7bee2a81cda7d2e0f8f784dfa
Quentin Guillemin <quentin.guillemin@recia.fr>
2026-04-22 10:20:57 +0200

    refactor(ui): split user structure

---
+++ src/main/webapp/src/components/user/tabs/information/UserStructure.vue
@@ -0,0 +1,139 @@
+<script setup lang="ts">
+import type { UserStructure } from '@/types/index.ts'
+import { faHouseUser, faLandmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { useI18n } from 'vue-i18n'
+import { RouterLink } from 'vue-router'
+
+defineProps<{
+  structure: UserStructure
+}>()
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="structure-card">
+    <div class="info">
+      <RouterLink
+        v-if="structure.authorizedForPrincipal"
+        :to="{
+          name: 'structure',
+          params: { structureId: structure.id },
+        }"
+      >
+        {{ structure.nom }}
+        <span aria-hidden="true" />
+      </RouterLink>
+      <p v-else>
+        {{ structure.nom }}
+      </p>
+
+      <p
+        v-if="structure.type || structure.uai"
+        class="description"
+      >
+        {{ structure.type }}
+        <span v-if="structure.uai">
+          {{ structure.uai }}
+        </span>
+      </p>
+    </div>
+
+    <div
+      v-if="
+        structure.structureRattachement
+          || structure.structureCourante
+      "
+      class="icons"
+    >
+      <span
+        v-show="structure.structureRattachement"
+        :title="t('page.user.structure.admin')"
+      >
+        <FontAwesomeIcon
+          :icon="faLandmark"
+          size="lg"
+        />
+      </span>
+      <span
+        v-show="structure.structureCourante"
+        :title="t('page.user.structure.current')"
+        class="current"
+      >
+        <FontAwesomeIcon
+          :icon="faHouseUser"
+          size="lg"
+        />
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use 'sass:map';
+@use '@gip-recia/ui/core/variables' as *;
+@use '@gip-recia/ui/functions' as *;
+@use '@gip-recia/ui/mixins' as *;
+
+.structure-card {
+  display: flex;
+  gap: 8px;
+  height: 100%;
+  padding: 16px;
+  position: relative;
+  border-radius: 10px;
+  box-shadow: var(--#{$prefix}shadow-neutral) HEXToRGBA($black, 0.1);
+  outline-color: transparent;
+  outline-offset: -1px;
+  transition:
+    outline 0.15s ease-out,
+    box-shadow 0.15s ease-out;
+
+  &:has(> .info > a) {
+    &:hover,
+    &:has(:focus-visible) {
+      outline: 2px solid var(--#{$prefix}primary);
+      box-shadow: var(--#{$prefix}shadow-hover) HEXToRGBA(var(--#{$prefix}primary), 0.2);
+    }
+  }
+
+  > .info {
+    flex: 1 1 auto;
+
+    > a {
+      @include unstyled-link;
+
+      &:focus-visible {
+        outline: none;
+      }
+
+      > span {
+        position: absolute;
+        z-index: 1;
+        inset: 0;
+      }
+    }
+
+    > .description {
+      opacity: 0.6;
+    }
+  }
+
+  > .icons {
+    flex: 0 0 auto;
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    align-items: center;
+    gap: 4px;
+
+    > * {
+      z-index: 1;
+    }
+
+    > .current {
+      grid-row: 2;
+    }
+  }
+}
+</style>
